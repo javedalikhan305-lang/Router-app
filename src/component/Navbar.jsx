9b2756301347e891baea4880638c053ae89d7f96
Javed <javedalikhan035@gmail.com>
2026-01-12 08:44:34 -0800

    first commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-   <nav className='bg-green-600 py-2 mx- flex item-center justify-between'>
+   <nav className='bg-green-600 py-2 mx-4 flex item-center justify-between'>
    <Link to="/"><h1 className='test-lg text-white font-bold '>React Router</h1></Link>
 
    <ul className='flex space-x-5'>
