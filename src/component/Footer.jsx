fc5919437d9cf7b1d40289bae680d9930274414c
Javed <javedalikhan035@gmail.com>
2026-01-12 09:02:07 -0800

    first commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
   <footer className='bg-gray-400 text-center py-2'>
-    <h3 className=' text-sm text-gray-600'>React Router 2026</h3>
+    <h3 className=' text-sm text-gray-800'>React Router 2026</h3>
   </footer>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-8 min-h-[90vh]'>
+    <div className='p-8 min-h-[87vh]'>
       <h1 className='text-center'>Home</h1>
     </div>
   )
